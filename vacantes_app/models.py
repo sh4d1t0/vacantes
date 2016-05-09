e18f278313abda22c0c536a133bca6fd15528cc5
victor <victor.castillo@prospectomedia.com>
2016-05-09 16:02:11 -0500

    Si como no

--- vacantes_app/models.py
+++ vacantes_app/models.py
@@ -16,7 +16,7 @@ class Vacantes(models.Model):
 	description = HTMLField()
 	skills = HTMLField()
 	lang = models.CharField(max_length=5, choices=LANGUAGES)
-	date = models.DateTimeField()
+	date = models.DateTimeField(blank=True)
 
 	def __unicode__(self):
 		return self.position
