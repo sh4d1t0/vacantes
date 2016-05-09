e7958896eb379c0807e276adbafa7768d6029bfe
victor <victor.castillo@prospectomedia.com>
2016-05-09 15:02:28 -0500

    SI

--- vacantes_app/models.py
+++ vacantes_app/models.py
@@ -8,15 +8,15 @@ from tinymce.models import HTMLField
 
 class Vacantes(models.Model):
 	LANGUAGES = (
-        ('es', u'Español'),
-        ('en', u'Inglés'),
-    )
-    is_active = models.BooleanField()
-    position = models.CharField()
-    descripion = HTMLField()
-    skills = HTMLField()
-    lang = models.CharField(max_length=1, choices=LANGUAGES)
-    date = models.DateTimeField()
+	    ('es', u'Español'),
+	    ('en', u'Inglés'),
+	)
+	is_active = models.BooleanField()
+	position = models.CharField()
+	descripion = HTMLField()
+	skills = HTMLField()
+	lang = models.CharField(max_length=1, choices=LANGUAGES)
+	date = models.DateTimeField()
 
-   class Meta:
-   	db_table = 'po_vacancy'
+	class Meta:
+		db_table = 'po_vacancy'
